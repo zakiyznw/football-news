e85121e60f330e01f6b67397433ba080c86ddfd9
zakiyznw <zakiynashrudin1st@gmail.com>
2025-09-03 17:28:12 +0700

    Complete tutorial 1: Django MVT implementation

--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.db import models
+
+class News(models.Model):
+    CATEGORY_CHOICES = [
+        ('transfer', 'Transfer'),
+        ('update', 'Update'),
+        ('exclusive', 'Exclusive'),
+        ('match', 'Match'),
+        ('rumor', 'Rumor'),
+        ('analysis', 'Analysis'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    news_views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_featured = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
+    
+    @property
+    def is_news_hot(self):
+        return self.news_views > 20
+        
+    def increment_views(self):
+        self.news_views += 1
+        self.save()
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2406496044',
+        'name': 'Zakiy Nashrudin Wahid',
+        'class': 'PBP B'
+    }
+
+    return render(request, "main.html", context)
